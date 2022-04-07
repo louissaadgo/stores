3a7f2c53681165d7e1127545f77984faa0c78610
louissaadgo <louissaadgo@gmail.com>
2022-04-07 20:26:32 +0300

    Added authtoken platform based middlewares

--- middlewares/admin.go
+++ middlewares/admin.go
@@ -1,1 +1,63 @@
 package middlewares
+
+import (
+	"stores/models"
+	"stores/token"
+	"stores/views"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AdminMiddleware(c *fiber.Ctx) error {
+	platform := models.Platform{}
+	err := c.BodyParser(&platform)
+
+	if err != nil {
+		response := models.Response{
+			Type: models.TypeErrorResponse,
+			Data: views.Error{
+				Error: "Invalid Data Types",
+			},
+		}
+		c.Status(400)
+		return c.JSON(response)
+	}
+	var authToken string
+	if platform.Platform == models.PlatformWeb {
+		authToken = c.Cookies("token")
+	} else if platform.Platform == models.PlatformMobile {
+		authToken = platform.AuthToken
+	} else {
+		response := models.Response{
+			Type: models.TypeErrorResponse,
+			Data: views.Error{
+				Error: "Invalid platform",
+			},
+		}
+		c.Status(400)
+		return c.JSON(response)
+	}
+
+	payload, isValid := token.VerifyPasetoToken(authToken)
+	if !isValid {
+		response := models.Response{
+			Type: models.TypeErrorResponse,
+			Data: views.Error{
+				Error: "Unauthorized",
+			},
+		}
+		c.Status(400)
+		return c.JSON(response)
+	}
+	if payload.UserType != models.TypeAdmin {
+		response := models.Response{
+			Type: models.TypeErrorResponse,
+			Data: views.Error{
+				Error: "Unauthorized",
+			},
+		}
+		c.Status(400)
+		return c.JSON(response)
+	}
+	//check if admin in db then pass request
+}
