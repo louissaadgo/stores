83cd3eb978d5fbe6a270d1573ea595599302ee44
louissaadgo <louissaadgo@gmail.com>
2022-04-07 20:04:09 +0300

    added platform struct

---
+++ models/platform.go
@@ -0,0 +1,6 @@
+package models
+
+type Platform struct {
+	Platform  string `json:"platform"`
+	AuthToken string `json:"auth_token"`
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,19 @@ package routes
 
 import (
 	"stores/controllers"
+	"stores/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Initialize(app *fiber.App) {
+	//Unauthorized routes
 	app.Post("/api/v1/auth/admin/signin", controllers.AdminSignin)
 	app.Post("/api/v1/auth/merchant/signup", controllers.MerchantSignup)
 	app.Post("/api/v1/auth/merchant/signin", controllers.MerchantSignin)
 	app.Post("/api/v1/auth/user/signup", controllers.UserSignup)
 	app.Post("/api/v1/auth/user/signin", controllers.UserSignin)
+
+	//Admin specific routes
+	app.Use(middlewares.AdminMiddleware)
 }
