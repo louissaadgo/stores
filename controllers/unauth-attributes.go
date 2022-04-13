5eb7abe0ce43b853be2cf169f784bdb76c09cc57
louissaadgo <louissaadgo@gmail.com>
2022-04-13 09:06:55 +0300

    updated attributes views

--- controllers/unauth-attributes.go
+++ controllers/unauth-attributes.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetAllAttributes(c *fiber.Ctx) error {
-	var attributes []models.Attribute
+	var attributes []views.AttributeResponse
 	rows, err := db.DB.Query(`SELECT id, name, created_at, updated_at FROM attributes;`)
 	if err != nil {
 		response := models.Response{
@@ -23,7 +23,7 @@ func GetAllAttributes(c *fiber.Ctx) error {
 	}
 	defer rows.Close()
 
-	var attribute models.Attribute
+	var attribute views.AttributeResponse
 	for rows.Next() {
 		if err := rows.Scan(&attribute.ID, &attribute.Name, &attribute.CreatedAt, &attribute.UpdatedAt); err != nil {
 			response := models.Response{
@@ -35,6 +35,34 @@ func GetAllAttributes(c *fiber.Ctx) error {
 			c.Status(400)
 			return c.JSON(response)
 		}
+		newRows, err := db.DB.Query(`SELECT id, name, attribute_id,created_at, updated_at FROM attribute_values WHERE attribute_id = $1;`, attribute.ID)
+		if err != nil {
+			response := models.Response{
+				Type: models.TypeErrorResponse,
+				Data: views.Error{
+					Error: "Something went wrong please try again",
+				},
+			}
+			c.Status(400)
+			return c.JSON(response)
+		}
+		defer newRows.Close()
+		var attValues []views.AttributeValueResponse
+		for newRows.Next() {
+			var attValue views.AttributeValueResponse
+			if err := newRows.Scan(&attValue.ID, &attValue.Name, &attValue.AttributeID, &attValue.CreatedAt, &attValue.UpdatedAt); err != nil {
+				response := models.Response{
+					Type: models.TypeErrorResponse,
+					Data: views.Error{
+						Error: "Something went wrong please try again",
+					},
+				}
+				c.Status(400)
+				return c.JSON(response)
+			}
+			attValues = append(attValues, attValue)
+		}
+		attribute.Values = attValues
 		attributes = append(attributes, attribute)
 	}
 
--- views/attribute.go
+++ views/attribute.go
@@ -1,7 +1,21 @@
 package views
 
-import "stores/models"
+type AttributeValueResponse struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	AttributeID string `json:"attribute_id"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type AttributeResponse struct {
+	ID        string                   `json:"id"`
+	Name      string                   `json:"name"`
+	Values    []AttributeValueResponse `json:"values"`
+	CreatedAt string                   `json:"created_at"`
+	UpdatedAt string                   `json:"updated_at"`
+}
 
 type AllAttributes struct {
-	Attributes []models.Attribute `json:"attributes"`
+	Attributes []AttributeResponse `json:"attributes"`
 }
